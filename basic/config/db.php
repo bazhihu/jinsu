5df542f585cdee4cf587efb83afbea5e71d7751d
herui <h2000r@qq.com>
2015-03-07 19:14:43 +0800

    first commit

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'youaiyihu',
+    'password' => '123456',
+    'charset' => 'utf8',
+];

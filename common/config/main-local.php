6f18efeed9a5a850f51bf3f1cb0d6bcae76f5c2a
yuliangliang <yuliangliang@youaiyihu.com>
2015-03-09 20:17:13 +0800

    edit common config

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=192.168.1.20;dbname=yii2advanced',
+            'username' => 'youaiyihu',
+            'password' => '12345678',
             'charset' => 'utf8',
         ],
         'mailer' => [

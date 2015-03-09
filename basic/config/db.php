2005a4be171a216f9af633f50ebb54fb24a435e9
zhangbo <zhangbo@youaiyihu.com>
2015-03-09 20:33:51 +0800

    delete

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'youaiyihu',
-    'password' => '123456',
-    'charset' => 'utf8',
-];

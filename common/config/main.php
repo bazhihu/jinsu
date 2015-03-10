d8fbd85355dea7ec919568ff410b1cc042f2e829
zhangbo <zhangbo@youaiyihu.com>
2015-03-10 21:20:45 +0800

    配置redis

---
+++ common/config/main.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+//        'cache' => [
+//            'class' => 'yii\caching\FileCache',
+//        ],
+
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        'cache' => [
+            'class' => 'yii\redis\Cache',
+        ],
+        'session' => [
+            'class' => 'yii\redis\Session',
+        ],
+    ],
+];

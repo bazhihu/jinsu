c8f377698affa8c4dd6c00af2d583b506cf7b980
tianchunqing <tianchunqing@youaiyihu.com>
2015-03-31 13:51:34 +0800

    护工照片

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -14,42 +14,50 @@ use kartik\datecontrol\DateControl;
 
 <div class="user-form">
 
-    <?php $form = ActiveForm::begin(['type'=>ActiveForm::TYPE_HORIZONTAL]); echo Form::widget([
+    <?php $form = ActiveForm::begin(['type'=>ActiveForm::TYPE_HORIZONTAL]);
+    echo Form::widget([
+        'model' => $model,
+        'form' => $form,
+        'columns' => 1,
+        'attributes' => [
+            'mobile'=>[
+                'type'=> Form::INPUT_TEXT,
+                'options'=>[
+                    'placeholder'=>'请输入用户名（用户名即手机号）...',
+                    'maxlength'=>32,
+                    'style'=>'width:50%'
+                ]
+            ],
 
-    'model' => $model,
-    'form' => $form,
-    'columns' => 1,
-    'attributes' => [
-        'mobile'=>[
-            'type'=> Form::INPUT_TEXT,
-            'options'=>[
-                'placeholder'=>'请输入用户名（用户名即手机号）...',
-                'maxlength'=>32,
-                'style'=>'width:50%'
-            ]
-        ],
+            'name'=>[
+                'type'=> Form::INPUT_TEXT,
+                'options'=>[
+                    'placeholder'=>'请输入姓名...',
+                    'maxlength'=>50,
+                    'style'=>'width:50%'
+                ]
+            ],
 
-        'name'=>[
-            'type'=> Form::INPUT_TEXT,
-            'options'=>[
-                'placeholder'=>'请输入姓名...',
-                'maxlength'=>50,
-                'style'=>'width:50%'
-            ]
-        ],
-
-        'gender'=>[
-            'type'=> Form::INPUT_RADIO_LIST,
-            'options'=>['placeholder'=>'请选择性别...'], 'items'=>['男'=>'男','女'=>'女'], 'options'=>['inline'=>true]
-        ],
-
-        'status'=>[
-            'type'=> Form::INPUT_RADIO_LIST,
-            'options'=>['placeholder'=>'请选择账号状态...'], 'items'=>['1'=>'正常','2'=>'禁用'], 'options'=>['inline'=>true]
-        ],
-    ]
-
+            'gender'=>[
+                'type'=> Form::INPUT_RADIO_LIST,
+                'options'=>['placeholder'=>'请选择性别...'], 'items'=>['男'=>'男','女'=>'女'], 'options'=>['inline'=>true]
+            ],
+        ]
     ]);
+    if(!$model->isNewRecord){
+        echo Form::widget([
+            'model' => $model,
+            'form' => $form,
+            'columns' => 1,
+            'attributes' => [
+                'status'=>[
+                    'type'=> Form::INPUT_RADIO_LIST,
+                    'options'=>['placeholder'=>'请选择账号状态...'], 'items'=>['1'=>'正常','2'=>'禁用'], 'options'=>['inline'=>true]
+                ],
+            ]
+        ]);
+    }
+    //echo $form->field($model, 'pic')->inputOptions('IN'); ;
     echo Html::submitButton($model->isNewRecord ? Yii::t('app', '注册') : Yii::t('app', '编辑'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
     ActiveForm::end(); ?>
 

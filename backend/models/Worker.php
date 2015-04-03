faf0f122aaea82ede62aa0c00013c44cdff9498e
tianchunqing <tianchunqing@youaiyihu.com>
2015-04-03 16:51:26 +0800

    护工照片

--- backend/models/Worker.php
+++ backend/models/Worker.php
@@ -523,19 +523,10 @@ class Worker extends \yii\db\ActiveRecord
      * @param $pic 护工照片
      * @return string
      */
-    static  public  function  workerPic($pic){
+    static public function workerPic($pic){
         if($pic)
             return "uploads/".$pic;
         else
             return "uploads/no.jpg";
-    }
-
-    /**
-     * 通过护工编号取照片
-     * @param $worker_id 护工编号
-     */
-    static public  function  workerPicByWorkerId($worker_id){
-        $worker_info = Worker::findOne(['worker_id'=>$worker_id]);
-        return Worker::workerPic($worker_info->pic);
     }
 }

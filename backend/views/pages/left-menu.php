47755b57b9e250f16ec85b87b33275c0a966e32e
huzhiqang <1123456>
2015-04-09 16:30:34 +0800

    2015-4-9 No.2 菜单隐藏  胡志强

--- backend/views/pages/left-menu.php
+++ backend/views/pages/left-menu.php
@@ -7,40 +7,40 @@ $route = $controller->route;
 
 ?>
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'worker/create'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('worker/create');?>">添加护工</a></li>
-    <li <?php if($route == 'worker/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('worker/index');?>">护工管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/worker/create')):?>hide <?php endif;?><?php if($route == 'worker/create'):?>active<?php endif;?>"><a href="<?=Url::toRoute('worker/create');?>">添加护工</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/worker/index')):?>hide <?php endif;?><?php if($route == 'worker/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('worker/index');?>">护工管理</a></li>
 
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'order/create'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('order/create');?>">新增订单</a></li>
-    <li <?php if($route == 'order/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('order/index');?>">订单管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/order/create')):?>hide <?php endif;?><?php if($route == 'order/create'):?>active<?php endif;?>"><a href="<?=Url::toRoute('order/create');?>">新增订单</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/order/index')):?>hide <?php endif;?><?php if($route == 'order/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('order/index');?>">订单管理</a></li>
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'user/create'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('user/create');?>">用户注册</a></li>
-    <li <?php if($route == 'user/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('user/index');?>">用户管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/user/create')):?>hide <?php endif;?><?php if($route == 'user/create'):?>active<?php endif;?>"><a href="<?=Url::toRoute('user/create');?>">用户注册</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/user/index')):?>hide <?php endif;?><?php if($route == 'user/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('user/index');?>">用户管理</a></li>
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'wallet/recharge-records'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('wallet/recharge-records');?>">充值记录</a></li>
-    <li <?php if($route == 'wallet/cash-list'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('wallet/cash-list');?>">提现申请</a></li>
-    <li <?php if($route == 'wallet/confirm-list'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('wallet/confirm-list');?>">提现支付</a></li>
-    <li <?php if($route == 'wallet/cash-records'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('wallet/cash-records');?>">提现记录</a></li>
-    <li <?php if($route == 'wallet/debit-records'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('wallet/debit-records');?>">扣款明细</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/wallet/recharge-records')):?>hide <?php endif;?><?php if($route == 'wallet/recharge-records'):?>active<?php endif;?>"><a href="<?=Url::toRoute('wallet/recharge-records');?>">充值记录</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/wallet/cash-list')):?>hide <?php endif;?><?php if($route == 'wallet/cash-list'):?>active<?php endif;?>"><a href="<?=Url::toRoute('wallet/cash-list');?>">提现申请</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/wallet/confirm-list')):?>hide <?php endif;?><?php if($route == 'wallet/confirm-list'):?>active<?php endif;?>"><a href="<?=Url::toRoute('wallet/confirm-list');?>">提现支付</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/wallet/cash-records')):?>hide <?php endif;?><?php if($route == 'wallet/cash-records'):?>active<?php endif;?>"><a href="<?=Url::toRoute('wallet/cash-records');?>">提现记录</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/wallet/debit-records')):?>hide <?php endif;?><?php if($route == 'wallet/debit-records'):?>active<?php endif;?>"><a href="<?=Url::toRoute('wallet/debit-records');?>">扣款明细</a></li>
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'comment/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('comment/index');?>">评论管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/comment/index')):?>hide <?php endif;?><?php if($route == 'comment/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('comment/index');?>">评论管理</a></li>
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'admin-user/create'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('admin-user/create');?>">新增账号</a></li>
-    <li <?php if($route == 'admin-user/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('admin-user/index');?>">帐号列表</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/admin-user/create')):?>hide <?php endif;?><?php if($route == 'admin-user/create'):?>active<?php endif;?>"><a href="<?=Url::toRoute('admin-user/create');?>">新增账号</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/admin-user/index')):?>hide <?php endif;?><?php if($route == 'admin-user/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('admin-user/index');?>">帐号列表</a></li>
 </ul>
 
 <ul class="nav nav-sidebar">
-    <li <?php if($route == 'hospitals/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('hospitals/index');?>">医院管理</a></li>
-    <li <?php if($route == 'departments/index'):?>class="active"<?php endif;?>><a href="<?=Url::toRoute('departments/index');?>">科室管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/hospitals/index')):?>hide <?php endif;?><?php if($route == 'hospitals/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('hospitals/index');?>">医院管理</a></li>
+    <li class="<?php if(!\backend\models\AdminUser::checkPermissions('/departments/index')):?>hide <?php endif;?><?php if($route == 'departments/index'):?>active<?php endif;?>"><a href="<?=Url::toRoute('departments/index');?>">科室管理</a></li>
 </ul>
 

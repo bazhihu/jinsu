df69e6f6fad92e67e09298a3969c60f5aa10f3f8
zhangbo <zhangbo@youaiyihu.com>
2015-04-15 18:16:48 +0800

    修改配置

--- common/config/params.php
+++ common/config/params.php
@@ -1,5 +1,6 @@
 <?php
 $server_name = $_SERVER['SERVER_NAME'] ; //图片服务器
+$pic_domain = null;
 if($server_name=='admin.youaiyihu.com')
     $pic_domain = "admin.youaiyihu.com";
 if($server_name=='uat.admin.youaiyihu.com')
@@ -12,5 +13,15 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'pic_domain' =>$pic_domain //图片服务器
+    'pic_domain' =>$pic_domain, //图片服务器
+    'aliPay' => [
+        'partner' => '2088911035311653',
+        'seller_email' => 'pay@youaiyihu.com',
+        'ali_public_key_path' => 'rsa_public_key.pem',
+        'private_key_path' => 'rsa_public_key.pem',
+        'sign_type' => 'RSA',
+        'input_charset' => 'utf-8',
+        'cacert' => 'alipay_cacert.pem',
+        'transport' => 'http'
+    ]
 ];

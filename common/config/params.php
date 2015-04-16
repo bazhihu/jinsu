0509af64ee7a0761cc1d8cac5f6094556d65baf5
zhangbo <zhangbo@youaiyihu.com>
2015-04-16 20:11:27 +0800

    第三方支付

--- common/config/params.php
+++ common/config/params.php
@@ -17,8 +17,8 @@ return [
     'aliPay' => [
         'partner' => '2088911035311653',
         'seller_email' => 'pay@youaiyihu.com',
-        'ali_public_key_path' => 'rsa_public_key.pem',
-        'private_key_path' => 'rsa_public_key.pem',
+        'ali_public_key_path' => 'alipay_public_key.pem',
+        'private_key_path' => 'rsa_private_key.pem',
         'sign_type' => 'RSA',
         'input_charset' => 'utf-8',
         'cacert' => 'alipay_cacert.pem',

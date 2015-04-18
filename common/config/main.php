7812063308135c0f7e4bfa470e05fedee68799f9
herui <h2000r@qq.com>
2015-04-18 15:26:03 +0800

    main-local

--- common/config/main.php
+++ common/config/main.php
@@ -2,30 +2,12 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-
-//        'cache' => [
-//            'class' => 'yii\caching\FileCache',
-//        ],
-
-        'redis' => [
-            'class' => 'yii\redis\Connection',
-            'hostname' => '192.168.1.20',
-            'port' => 6379,
-            'database' => 0,
-        ],
         'cache' => [
             'class' => 'yii\redis\Cache',
         ],
         'session' => [
              'class' => 'yii\redis\Session'
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.1.20;dbname=youaiyihu',
-            'username' => 'youaiyihu',
-            'password' => '12345678',
-            'charset' => 'utf8',
-            'tablePrefix' => 'yayh_'
-        ]
+
     ],
 ];

3a95b19711da1fcccb42802e296ab8908b4bd2f8
zhangbo <zhangbo@youaiyihu.com>
2015-04-20 20:22:05 +0800

    图片域名

--- common/config/params-local.php
+++ common/config/params-local.php
@@ -1,15 +1,5 @@
 <?php
-$serverName = $_SERVER['SERVER_NAME'] ; //图片服务器
-$picDomain = null;
-if($serverName=='admin.youaiyihu.com')
-    $picDomain = 'admin.youaiyihu.com';
-if($serverName=='uat.admin.youaiyihu.com')
-    $picDomain = 'uat.img.youaiyihu.com';
-if($serverName=='sit.admin.youaiyihu.com')
-    $picDomain = 'sit.admin.youaiyihu.com';
-if($serverName=='dev.admin.youaiyihu.com')
-    $picDomain = 'dev.admin.youaiyihu.com';
 
 return [
-    'pic_domain' => $picDomain, //图片服务器
+    'pic_domain' => 'img.youaiyihu.com', //图片服务器
 ];

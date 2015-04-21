c71a79892d07baf0bc76d1022e5d12eeb274415a
zhangbo <zhangbo@youaiyihu.com>
2015-04-21 21:54:43 +0800

    微信支付回调

---
+++ api/modules/wechat/Module.php
@@ -0,0 +1,18 @@
+<?php
+namespace api\modules\wechat;
+
+/**
+ * iKargo API alipay Module
+ * 
+ * @author Budi Irawan <budi@ebizu.com>
+ * @since 1.0
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'api\modules\wechat\controllers';
+
+    public function init()
+    {
+        parent::init();        
+    }
+}
---
+++ api/modules/wechat/models/Notify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangbo
+ * Date: 2015/4/9
+ * Time: 17:30
+ */
+
+namespace api\modules\wechat\models;
+
+use Yii;
+
+class Notify{
+
+}

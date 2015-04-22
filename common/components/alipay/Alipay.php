5fd52f9ba7f08b6ed6e9b1016530b80132268160
zhangbo <zhangbo@youaiyihu.com>
2015-04-22 20:53:22 +0800

    支付回调

--- common/components/alipay/Alipay.php
+++ common/components/alipay/Alipay.php
@@ -11,11 +11,12 @@ namespace common\components\alipay;
 use Yii;
 
 class Alipay {
-    public static $notifyUrl = 'http://uat.api.youaiyihu.com/alipay/notify';
+
+    public static $notifyUrl;
     private $_config = null;
 
     public function __construct(){
         $this->_config = Yii::$app->params['aliPay'];
-
+        self::$notifyUrl = 'http://'.Yii::$app->getRequest()->serverName.'/alipay/notify';
     }
 }

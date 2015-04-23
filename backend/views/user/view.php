3812ee548e407a87e5509cb03ecc5603b7218253
sunliang <sunliang@youaiyihu.com>
2015-04-23 16:09:08 +0800

    tq-没登陆弹屏

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <h3 class="panel-title">操作</h3>
         </div>
         <div class="panel-body">
-            <?
+            <?php
             echo Html::a('<span class="btn  btn-primary">充值</span>', Yii::$app->urlManager->createUrl(['wallet/recharge','uid' => $model->id]), ['title' => '充值']);
             if(WalletUser::findOne(['uid'=>$model->id])){
                 $finace_status = WalletWithdrawcash::getWalletStatusByUid($model->id);

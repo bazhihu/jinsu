a868f8d882ece706f88e2b25f62453f024054b22
huzhiqang <1123456>
2015-04-24 11:35:02 +0800

    h5

--- m/js/login/login.js
+++ m/js/login/login.js
@@ -1,44 +1,57 @@
-/**
- * Created by HZQ on 2015/4/23.
- */
-(function(){
-    //登录
-    var submit = $('.submit'),
-        code = $('.code'),
-        postUrl = 'http://api.youaiyihu.com/v1/logins',
-        getUrl = 'http://api.youaiyihu.com/v1/sms';
-
-    submit.on('tap',function(e){
-        var data = $('form').serializeArray();
-        if(!validateLogin(data)){
-            return false;
+var submit = $('.submit'),
+    code = $('.code');
+var user = getStatus();
+submit.on(CLICK,function(err){
+    var data = $('form').serializeArray(),
+        error = true;
+    validateLogin(data, function(a){
+        if(!a)
+            error = false;
+    });
+    if(!error)
+        return false;
+    $.post(loginUrl,data,function(back){
+        if(back.code == 200)
+        {
+            setCookie(ID, back.data.uid);
+            setCookie(TOKEN, back.data.token);
+            window.location.href = history.go(-1);
         }
-        $.post(postUrl,data,function(response){
-            console.log(response);
-        });
     });
-    //发送验证码
-    code.on('tap',function(e){
-        var data = $('form').serializeArray();return false;
-        $.post(getUrl,data,function(response){
-            console.log(response);
-        });
+});
+code.on(CLICK,function(err){
+    var data = $('form').serializeArray(),
+        error = true;
+    validateSms(data, function(a){
+        if(!a)
+            error = false;
     });
-    //登录数据验证
-    function validateLogin(date){
-        $.map(date,function(item,index){
-            if(!item.value){
-                return false;
-            }
-        });
-        return true;
-    }
-    //短信数据验证
-    function validateSms(date){
-        $.map(date,function(item,index){
-            if(item){
-
-            }
-        });
-    }
-})(Zepto);
+    if(!error)
+        return false;
+    $.post(smsUrl,data,function(response){
+        console.log(response);
+    });
+});
+function validateLogin(date, callback){
+    var phone = /^(1[3|5|7|8|][0-9]{9})$/,
+        code = /^d{6}$/;
+    $.map(date,function(item,index){
+        if(item.name == 'mobile' && !phone.test(item.value)){
+            alert('请输入正确的手机号码');
+            callback(false);
+        }
+        if(item.name == 'authCode' && !code.test(item.value)){
+            alert('请输入正确的验证码');
+            callback(false);
+        }
+    });
+}
+function validateSms(date, callback){
+    var phone = /^(1[3|5|7|8|][0-9]{9})$/;
+    $.map(date,function(item,index){
+        if(item.name == 'mobile' && !phone.test(item.value)){
+            alert('请输入正确的手机号码');
+            callback(false);
+        }
+    });
+}

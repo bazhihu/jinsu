19ca429d7df51b8397d5f07d957d3a05050721c4
tianchunqing <tianchunqing@youaiyihu.com>
2015-04-25 14:45:14 +0800

    h5欢迎，问题

--- m/js/worker/worker.js
+++ m/js/worker/worker.js
@@ -1,5 +1,5 @@
 ;(function($){
-    $.getJSON('//sit.api.youaiyihu.com/v1/workers', function(back_data){
+    $.getJSON(workerUrl, function(back_data){
         var body_html = template('body_template', back_data);
         $('#body').html(body_html);
     })

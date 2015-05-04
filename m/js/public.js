ad93d2383fc58ea46f4f6aae4ae96ab94ad01841
tianchunqing <tianchunqing@youaiyihu.com>
2015-05-04 18:06:19 +0800

    my　订单详情

--- m/js/public.js
+++ m/js/public.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 var host = "http://"+window.location.host,
     UA = window.navigator.userAgent,
     CLICK = 'click',
-    url = 'http://api.youaiyihu.com/',
+    url = 'http://dev.api.youaiyihu.com/',
     version = 'v1/',
     ID = 'SID',
     NAME = 'name',
@@ -145,4 +145,71 @@ function getUsers(id, token, callback){
                 callback(false);
         })
     })(Zepto);
+}
+
+/**
+ * 获取url 参数
+ * @param name
+ * @returns {*}
+ */
+function getUrlQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+/**
+ * 时间差days
+ * @param startTime
+ * @param endTime
+ * @returns {number}
+ */
+function getOrderCycle(startTime,endTime){
+    var year1 =  startTime.substr(0,4);
+    var year2 =  endTime.substr(0,4);
+    var month1 = startTime.substr(5,2);
+    var month2 = endTime.substr(5,2);
+    var day1 = startTime.substr(8,2);
+    var day2 = endTime.substr(8,2);
+    var date1=new Date(year1,month1,day1);    //开始时间
+    var date2=new Date(year2,month2,day2);    //结束时间
+    var date3=date2.getTime()-date1.getTime()  //时间差的毫秒数
+    var days=parseInt(date3/(24*3600*1000));
+    return days;
+}
+
+/**
+ * 时间对象的格式化
+ */
+Date.prototype.format = function(format)
+{
+    /*
+     * format="yyyy-MM-dd hh:mm:ss";
+     */
+    var o = {
+        "M+" : this.getMonth() + 1,
+        "d+" : this.getDate(),
+        "h+" : this.getHours(),
+        "m+" : this.getMinutes(),
+        "s+" : this.getSeconds(),
+        "q+" : Math.floor((this.getMonth() + 3) / 3),
+        "S" : this.getMilliseconds()
+    }
+
+    if (/(y+)/.test(format))
+    {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4
+        - RegExp.$1.length));
+    }
+
+    for (var k in o)
+    {
+        if (new RegExp("(" + k + ")").test(format))
+        {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                ? o[k]
+                : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
 }

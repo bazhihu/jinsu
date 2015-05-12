68f1467fa470ce8ae68a0cd5adef176f43953cd6
herui <h2000r@qq.com>
2015-05-12 21:51:28 +0800

    护工列表页、详情页

--- m/js/worker/worker.js
+++ m/js/worker/worker.js
@@ -16,20 +16,18 @@ function ages(str)
     }
     return(false);
 }
+
 //护工等级
 var worker_levels = new Array();
+getConfigs(function(configs) {
+    worker_levels=configs.worker_levels;
+});
 function get_workelist() {
     /*
-    $.getJSON(configUrl, function (response) {
-        if (response.code == 200) {
-            //alert(response.data.worker_levels[0].name);
-            worker_levels=response.data.worker_levels;
-        };
-    });
-*/
     getConfigs(function(configs) {
         worker_levels=configs.worker_levels;
     });
+    */
     var start_time = getUrlQueryString("start_time");
     var hospital_id = getUrlQueryString("hospital_id");
     var department_id = getUrlQueryString("department_id");

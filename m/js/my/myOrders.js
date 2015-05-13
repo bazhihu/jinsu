f5b1cef89d04c07aabd0b6fa99bd93485e9ec975
huzhiqang <1123456>
2015-05-13 16:46:02 +0800

    h5

--- m/js/my/myOrders.js
+++ m/js/my/myOrders.js
@@ -7,42 +7,46 @@ $.getJSON(url,function(response){
         var backData = response.data,
             lenth =backData['items'].length,
             msg = new Array();
-        for(var i =0;i<=lenth-1;i++){
-            var item = new Array(),
-                value = backData['items'][i];
-            if(value['start_time'] && value['end_time']){
-                value['days'] = getOrderCycle(value['start_time'], value['end_time']);
+        if(backData.items.length>0){
+            for(var i =0;i<=lenth-1;i++){
+                var item = new Array(),
+                    value = backData['items'][i];
+                if(value['start_time'] && value['end_time']){
+                    value['days'] = getOrderCycle(value['start_time'], value['end_time']);
+                }
+                else
+                    value['days']  ='';
+                if(value['order_status'] == 'wait_pay'){
+                    value['order_status'] = '待支付';
+                    value['div_status'] = 'status-unpaid';
+                }else if(value['order_status'] == 'wait_confirm'){
+                    value['order_status'] = '待服务';
+                    value['div_status'] = 'status-not-start';
+                }else if(value['order_status'] == 'wait_service'){
+                    value['order_status'] = '待服务';
+                    value['div_status'] = 'status-not-start';
+                }else if(value['order_status'] == 'in_service'){
+                    value['order_status'] = '服务中';
+                    value['div_status'] = 'status-being-services';
+                }else if(value['order_status'] == 'end_service'){
+                    value['order_status'] = '结束服务';
+                    value['div_status'] = 'status-wait-review';
+                }else if(value['order_status'] == 'cancel'){
+                    value['order_status'] = '取消订单';
+                    value['div_status'] = 'status-cancel';
+                }else if(value['order_status'] == 'end_evaluate'){
+                    value['order_status'] = '结束评价';
+                    value['div_status'] = 'status-completed';
+                }else if(value['order_status'] == 'wait_evaluate'){
+                    value['order_status'] = '待评价';
+                    value['div_status'] = 'status-wait-review';
+                }
             }
-            else
-                value['days']  ='';
-            if(value['order_status'] == 'wait_pay'){
-                value['order_status'] = '待支付';
-                value['div_status'] = 'status-unpaid';
-            }else if(value['order_status'] == 'wait_confirm'){
-                value['order_status'] = '待服务';
-                value['div_status'] = 'status-not-start';
-            }else if(value['order_status'] == 'wait_service'){
-                value['order_status'] = '待服务';
-                value['div_status'] = 'status-not-start';
-            }else if(value['order_status'] == 'in_service'){
-                value['order_status'] = '服务中';
-                value['div_status'] = 'status-being-services';
-            }else if(value['order_status'] == 'end_service'){
-                value['order_status'] = '结束服务';
-                value['div_status'] = 'status-wait-review';
-            }else if(value['order_status'] == 'cancel'){
-                value['order_status'] = '取消订单';
-                value['div_status'] = 'status-cancel';
-            }else if(value['order_status'] == 'end_evaluate'){
-                value['order_status'] = '结束评价';
-                value['div_status'] = 'status-completed';
-            }else if(value['order_status'] == 'wait_evaluate'){
-                value['order_status'] = '待评价';
-                value['div_status'] = 'status-wait-review';
-            }
+            var bodyHtml = template('bodyTemplate', backData);
+            $('#body').html(bodyHtml);
+        }else{
+            $('#body').html('<div style="padding: 30px;"><span style="position: absolute;left:30%;height:200px;line-height:200px;overflow:hidden;">还没有任何订单哦：（</span></div>');
         }
-        var bodyHtml = template('bodyTemplate', backData);
-        $('#body').html(bodyHtml);
     }
 });
 item.live(CLICK, function(e){

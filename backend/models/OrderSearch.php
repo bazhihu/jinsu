96212724a8dfcd11a72634d7c8a15a82f4379058
zhangbo <zhangbo@youaiyihu.com>
2015-05-15 16:28:09 +0800

    订单管理优化

--- backend/models/OrderSearch.php
+++ backend/models/OrderSearch.php
@@ -15,9 +15,10 @@ class OrderSearch extends OrderMaster
     public function rules()
     {
         return [
-            [['order_no', 'uid', 'hospital_id', 'patient_state', 'worker_level', 'worker_no'], 'integer'],
-            [['order_no', 'mobile', 'start_time', 'end_time', 'reality_end_time', 'order_status', 'worker_name'], 'safe'],
+            [['order_no', 'uid', 'hospital_id', 'worker_level', 'worker_no'], 'integer'],
+            [['order_no', 'mobile', 'patient_name', 'start_time', 'end_time', 'reality_end_time', 'order_status', 'worker_name'], 'safe'],
             [['total_amount'], 'number'],
+            [['start_time', 'end_time'], 'required', 'on'=>'chart'],
         ];
     }
 
@@ -64,15 +65,60 @@ class OrderSearch extends OrderMaster
             'total_amount' => $this->total_amount,
             'patient_state' => $this->patient_state,
             'worker_no' => $this->worker_no,
-            'worker_name' => $this->worker_name,
             'mobile' => $this->mobile,
             'order_status' => $this->order_status
         ]);
 
         $query->andFilterWhere(['>=', 'start_time', $this->start_time])
-            ->andFilterWhere(['<=', 'end_time', $this->end_time]);
+            ->andFilterWhere(['<=', 'end_time', $this->end_time])
+            ->andFilterWhere(['like', 'worker_name', $this->worker_name])
+            ->andFilterWhere(['like', 'patient_name', $this->patient_name]);
 
         //!isset($params['sort']) && $query->orderBy('reality_end_time ASC');
         return $dataProvider;
+    }
+
+    public function chart($params){
+        $params = isset($params['OrderSearch']) ? $params['OrderSearch'] : null;
+        if(empty($params['start_time'])){
+            $this->start_time = date('Y-m-d', strtotime(date('Y-m-d'))-(86400*30));
+        }else{
+            $this->start_time = $params['start_time'];
+        }
+        if(empty($params['end_time'])){
+            $this->end_time = date('Y-m-d');
+        }else{
+            $this->end_time = $params['end_time'];
+        }
+        $result = [];
+        $connection = Yii::$app->db;
+
+        //每天的订单量
+        $column = "count(*) as total,DATE_FORMAT(create_time,'%Y-%m-%d') as date";
+        $where = "create_time>='$this->start_time' AND create_time<='$this->end_time'";
+        $groupBy = "DATE_FORMAT(create_time,'%Y-%m-%d')";
+        $sql = "SELECT $column FROM yayh_order_master WHERE $where GROUP BY $groupBy";
+        //echo $sql;exit;
+        $command = $connection->createCommand($sql);
+        $result['all'] = $command->queryAll();
+
+
+        //移动端
+        $column = "count(*) as total,DATE_FORMAT(create_time,'%Y-%m-%d') as date";
+        $where = "create_time>='$this->start_time' AND create_time<='$this->end_time' AND create_order_sources='".OrderMaster::ORDER_SOURCES_MOBILE."'";
+        $groupBy = "DATE_FORMAT(create_time,'%Y-%m-%d')";
+        $sql = "SELECT $column FROM yayh_order_master WHERE $where GROUP BY $groupBy";
+        $command = $connection->createCommand($sql);
+        $result['mobile'] = $command->queryAll();
+
+        //后台
+        $column = "count(*) as total,DATE_FORMAT(create_time,'%Y-%m-%d') as date";
+        $where = "create_time>='$this->start_time' AND create_time<='$this->end_time' AND create_order_sources='".OrderMaster::ORDER_SOURCES_SERVICE."'";
+        $groupBy = "DATE_FORMAT(create_time,'%Y-%m-%d')";
+        $sql = "SELECT $column FROM yayh_order_master WHERE $where GROUP BY $groupBy";
+        $command = $connection->createCommand($sql);
+        $result['service'] = $command->queryAll();
+        return $result;
+
     }
 }

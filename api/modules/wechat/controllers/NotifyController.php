d5fdeac9a7fb625ca45704a878bd458f6721eb52
huzhiqang <1123456>
2015-05-17 16:14:12 +0800

    微信log

--- api/modules/wechat/controllers/NotifyController.php
+++ api/modules/wechat/controllers/NotifyController.php
@@ -37,7 +37,9 @@ class NotifyController extends ActiveController{
      */
     public function actionCreate(){
         $notify = new Notify();
-        $xml = $GLOBALS['HTTP_RAW_POST_DATA'];var_dump($xml);Yii::info('wechat:'.$xml, 'api');
+        $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
+        var_dump($xml);
+        Yii::info('返回微信:'.$xml, 'api');
         $notify->notifyUrl();
     }
     public function actionIndex(){

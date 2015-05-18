b96247e41dd6c1026c89470410aebfe97973d625
tianchunqing <tianchunqing@youaiyihu.com>
2015-05-18 11:41:37 +0800

    订单

--- m/js/order/order.js
+++ m/js/order/order.js
@@ -19,16 +19,15 @@
     $('a[target="popup"]')['click'](function (e){
 		e.preventDefault();
 		var a = this, frame;
-		if (a.popupFrameId) {
-			frame = document.getElementById('popup-frame-' + a.popupFrameId);
-		} else {
+		//if (a.popupFrameId) {
+		//	frame = document.getElementById('popup-frame-' + a.popupFrameId);
+		//} else {
 			a.popupFrameId = ++popupFrameId;
 			frame = document.createElement('iframe');
 			frame.id = 'popup-frame-' + popupFrameId;
 			frame.className = 'popup-frame';
-		}
+		//}
         frame.src = a.href;
-        console.log(a.href)
 		$(frame).popup('right', false, false);
 		$(frame).one('load', function () {
 			frame.contentWindow.setValueByHash && frame.contentWindow.setValueByHash(a.querySelector('input[type="hidden"]').value);

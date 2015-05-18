0855fc8ca6d671ee4aca33435e3bd6ba383ae1a0
herui <h2000r@qq.com>
2015-05-18 21:39:46 +0800

    增加了微信支付

--- m/js/order/orderConfirm.js
+++ m/js/order/orderConfirm.js
@@ -249,7 +249,7 @@ getConfigs(function(configs) {
                         timeout:30000,
                         success: function(data){
                             if(data.code ==200){
-                                self.location = '/my/wechat.php??orderNo='+data.data.order.order_no+'&totalAmount='+data.data.order.total_amount+'&walletMoney='+walletMoney+"&nonce_str='+data.data.payment.nonce_str;
+                                self.location = '/my/wechat.php??orderNo='+data.data.order.order_no+'&totalAmount='+data.data.order.total_amount+'&walletMoney='+walletMoney+'&nonce_str='+data.data.payment.nonce_str;
                             }
                         },
                         error: function(xhr, type){

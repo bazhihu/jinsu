38d4daafae241aee026fe5321b75e346e70142a4
huzhiqang <1123456>
2015-05-19 15:05:12 +0800

    微信支付

--- m/my/wechat.php
+++ m/my/wechat.php
@@ -124,6 +124,7 @@ $jsApiParameters = $tools->GetJsApiParameters($order);
             <h3 class="menu-title">微信支付</h3>
         </header>
     </section>
+
 <!--<div id="foot"></div>-->
 </body>
 </html>

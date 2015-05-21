6dc069281611d50b843b7a435529aad601b8bcde
huzhiqang <1123456>
2015-05-21 14:47:29 +0800

    微信支付

--- m/my/wechat.php
+++ m/my/wechat.php
@@ -10,13 +10,6 @@ require_once WEB_ROOT."/common/components/wxpay/unit/log.php";
 $notifyUrl = '';
 $totalAmount=$_REQUEST["totalAmount"];
 $needPay=$_REQUEST["totalAmount"]-$_REQUEST["walletMoney"];
-if($_SERVER["HTTP_HOST"] !="m.youaiyihu.com"){
-    $notifyUrl = 'http://uat.m.youaiyihu.com/my/notify.php';
-    $needPay = 1;
-}else{
-    $needPay = $needPay*100;
-    $notifyUrl = 'http://m.youaiyihu.com/my/notify.php';
-}
 
 //获取用户openid
 $tools = new JsApiPay();
@@ -29,7 +22,15 @@ $input->SetBody("优爱医护订单");
 $input->SetOut_trade_no($_REQUEST["orderNo"]);
 //$input->SetOut_trade_no($_REQUEST["orderNo"]);
 //$input->SetTotal_fee($totalAmount);
-$input->SetTotal_fee($needPay);
+
+if($_SERVER["HTTP_HOST"] !="m.youaiyihu.com"){
+    $notifyUrl = 'http://uat.m.youaiyihu.com/my/notify.php';
+    $input->SetTotal_fee(1);
+}else{
+    $input->SetTotal_fee($needPay*100);
+    $notifyUrl = 'http://m.youaiyihu.com/my/notify.php';
+}
+
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");

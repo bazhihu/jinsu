4ac621a19cb96cb8ceab8eead2a651393bd688ac
huzhiqang <1123456>
2015-05-21 15:03:29 +0800

    微信支付

--- m/js/public.js
+++ m/js/public.js
@@ -310,6 +310,7 @@ function convertArray(o) {
 
 //页面返回上一页统一处理
 var user = getStatus();
+/*
 if(!document.referrer || (document.referrer.match(/login.html/i) && user)){
     $('.back').attr('href','/');
-}
+}*/

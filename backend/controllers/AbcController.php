4b010bed4a9f5d7d347af4ee2a45a458fa8eeb31
huzhiqang <1123456>
2015-05-22 15:48:02 +0800

    护工图片脚本

--- backend/controllers/AbcController.php
+++ backend/controllers/AbcController.php
@@ -8,6 +8,7 @@
 namespace backend\controllers;
 
 
+use backend\models\Worker;
 use common\models\Sms;
 use common\models\UmengPush;
 use common\models\Wallet;
@@ -22,20 +23,22 @@ class AbcController extends Controller
 
     public function actionSend(){
 
-
+        var_dump($_SERVER["HTTP_HOST"]);exit;
+/*        echo urlencode('http://uat.api.youaiyihu.com/wechat/notify');
+exit;*/
         /*echo urlencode('mxHWBmEFEqi7A6dTpVCrZzVmMGYyZDI0ZDU5ZDIwZWMzMjdmYTk0MWU4MmFjMmE2MzJlZjBmNTE1Mzg0N2YwMTUwNDEyNGE5MzRjOGU2MTLvO+we20rwSxm/f24MjjDCdvs04yZxwUBd5nwWtaviBLrRb9LqSDOLoudfyP6985TfyopEsziiuT43sgziHhx7');
         exit;
-        echo urlencode('1gbcBOR9qz5K2J8+4f9+aTkwZjFmOGVjMjQ3NTlkNGY3YmQ5YjI0NmM5NTBkMjNhMzc2MTZhNGU3NWYwYTUyNzI4NGYzNWMxNzk4MjM1NTiAo2cMh4x7QzL16uFgEUeEJnqTU2AqXde8VchtDIj3TCi17SzS4t+YmT/mwEAOX/oLqXjiukA9mlLh4uXoZr7B');
+        echo urlencode('hS0v+spF59eVO0GGTaklBTFjYTZkMWYyMjM3MWFjNzA3ZDYzM2I4MWFjNTAwYmM0YTQ2NmZlM2VjZTkwNGRiNjM2MTUyOWNlNmQwYzM4NDKAGR77LfCv+dTgvp98F8FFbPoyOiyxO5PMVJg3RWdWBMedbgfB5SYsUldqETxtueQey4y80tG0h38gsi8FB6sL');
         exit;*/
 
        /* $res = Wallet::getBalance(30);
         var_dump($res);exit;*/
 
-        $res = Wallet::getBalance(35);
+        /*$res = Wallet::getBalance(35);
         var_dump($res);
         exit;
 
-        $sms = new Sms();
+        $sms = new Sms();*/
 
         /*$string = '2015-04-14 00:00:00';
 
@@ -43,7 +46,7 @@ class AbcController extends Controller
         var_dump($var);exit;*/
 
         //$res = $sms->send('18810987761','154785');
-        $params = [
+        /*$params = [
             //'mobile'=>'18810987761,13911037234,18610308130,18942674005,17600819859',
             'mobile'=>'18810987761',
             'account'=>'13911037234',
@@ -57,9 +60,26 @@ class AbcController extends Controller
             'balance'=>'10000',
         ];
         $res = $sms->send($params);
-        var_dump($res);
+        var_dump($res);*/
     }
 
+    /**
+     * 护工头像判断
+     */
+    public function actionCarer(){
+        $carer = new Worker();
+        $all = $carer::find()->all();
+        foreach($all as $key=>$val){
+            $picId = $val->worker_id;
+            $picPath = 'uploads/'.$picId.'.jpg';
+            if(file_exists($picPath)){
+                $val->pic = $picId;
+            }else{
+                $val->pic = '';
+            }
+            $val->save();
+        }
+    }
     public function actionPush(){
 
         $push = new UmengPush();

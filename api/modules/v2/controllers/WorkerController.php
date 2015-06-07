18323a8577a630d1f661c7554c88db804bdc6bfc
zhangbo <zhangbo@youaiyihu.com>
2015-06-07 22:51:14 +0800

    二期

--- api/modules/v2/controllers/WorkerController.php
+++ api/modules/v2/controllers/WorkerController.php
@@ -80,6 +80,7 @@ class WorkerController extends ActiveController {
         #护工评价
         $worker['comments'] = Comment::find()
             ->andFilterWhere(['worker_id'=>$workerId, 'status' => Comment::STATUS_AUDIT_OK])
+            ->andFilterWhere(['<>', 'content', ''])
             ->orderBy('comment_id DESC')
             ->limit(self::$commentOffset)
             ->all();

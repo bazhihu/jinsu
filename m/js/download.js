4c1e4c6c98d311b3022b02d18dfe7a052c9e544a
tianchunqing <tianchunqing@youaiyihu.com>
2015-06-09 11:51:00 +0800

    苹果下载

--- m/js/download.js
+++ m/js/download.js
@@ -1,24 +1,24 @@
 $('.download').on(CLICK,function(){
     var ifrSrc="yayhapp://cn.md.dt/";//APP协议头，由APP开发人员定义提供
     if(navigator.userAgent.toLowerCase().match(/(iphone|ipad|ipod|ios)/i)){
-        document.location ="downloadIos.html";
-//                //IOS
-//                var hasApp = true;//是否安装了app
-//                var t1 = Date.now();
-//                var ifr = document.createElement('iframe');
-//                ifr.src = ifrSrc ;
-//                ifr.style.display = 'none';
-//                document.body.appendChild(ifr);
-//                setTimeout(function(){
-//                    document.body.removeChild(ifr);
-//                },1000);
-//                var t2 = Date.now();
-//                if(t2-t1<100){
-//                    hasApp = false;
-//                }
-//                if(!hasApp){//如果没安装，再打开下载地址
-//                    document.location = "跳转到appstore地址";
-//                }
+        //document.location ="downloadIos.html";
+                //IOS
+                var hasApp = true;//是否安装了app
+                var t1 = Date.now();
+                var ifr = document.createElement('iframe');
+                ifr.src = ifrSrc ;
+                ifr.style.display = 'none';
+                document.body.appendChild(ifr);
+                setTimeout(function(){
+                    document.body.removeChild(ifr);
+                },1000);
+                var t2 = Date.now();
+                if(t2-t1<100){
+                    hasApp = false;
+                }
+                if(!hasApp){//如果没安装，再打开下载地址
+                    document.location = "https://itunes.apple.com/cn/app/you-ai-yi-hu-nin-tie-xin-hu/id993561967?mt=8";
+                }
     }else if(navigator.userAgent.toLowerCase().match(/MicroMessenger/i)=="micromessenger") {
         //微信
         $('#weixin').show();

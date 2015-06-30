e1a5bae96efe28dffc2b2ef75542ba97688ba6ff
huzhiqang <1123456>
2015-06-30 11:16:57 +0800

    搜索

--- m/js/search.js
+++ m/js/search.js
@@ -42,21 +42,56 @@
         }
 
         function search() {
-            var searchUrl = '';
+            var searchUrl = workerUrl_v2+'?name='+input.value;
             // 这里添加搜索的逻辑
+
             $.ajax({
                 data: {q: input.value} ,
                 type: "GET",
                 dataType: "json",
-                url: cancelUrl,
+                url: searchUrl,
                 async:false,
                 cache:false,
                 crossDomain:true,
                 timeout:30000,
                 success: function(data){
                     if(data.code == 200){
-                        list.querySelector('ul').innerHTML = result;
-                        window.location.reload();
+                        console.log(data.data);
+                        var items = data.data.items,
+                            length = items.length,
+                            list = $('#list');
+                        list.empty();
+                        for(var i = 0;i<length;i++){
+                            var result = '<li>' +
+                                '<a href="./nursesDetail.html?worker_id='+items[i]['worker_id']+'&start_time=0&end_time=0&in_service='+items[i]['in_service']+'">' +
+                                '<div class="nurses-photo" style="background-image: url('+items[i]['pic']+')"></div>' +
+                                '<div class="nurses-detail">' +
+                                '<span class="nurses-price"><em>'+items[i]['price']+'</em>元/天</span>';
+                            if(items[i].star>4 || !items[i].star){
+                                result+='<span class="nurses-rate">★★★★★</span>';
+                            } else if(items[i].star>3){
+                                result+='<span class="nurses-rate">★★★★</span>';
+                            }else if(items[i].star>2){
+                                result+='<span class="nurses-rate">★★★</span>';
+                            }else if(items[i].star>1){
+                                result+='<span class="nurses-rate">★★</span>';
+                            }else if(items[i].star>0){
+                                result+='<span class="nurses-rate">★</span>';
+                            }
+                            result +=
+                                '<h4 class="nurses-name"><span class="key">'+items[i]['name']+'</span></h4>' +
+                                '<p class="nurses-intro">'+ages(items[i].birth)+'岁 | '+items[i].native_province+' | '+ages(items[i].start_work)+'年从业经验</p>' +
+                                '<p class="nurses-reserve"><span class="care-level-special">'+items[i].level_name+'</span>';
+                            if(items[i].in_service)
+                                result+='<span class="status-busy nurses-status">服务中</span>';
+                            else
+                                result+='<span class="nurses-status">空闲</span>';
+
+                            result+='</div></a></li>';
+                            list.append(result);
+                        }
+                        /*list.querySelector('ul').innerHTML = result;*/
+                        //window.location.reload();
                     }
                 },
                 error: function(jqXHR, textStatus, errorThrown){
@@ -77,5 +112,25 @@
             history.querySelector('ul').innerHTML = '';
             this.style.display = 'none';
         });
+    }
+    /**
+     * 计算年龄
+     * @param str
+     * @returns {*}
+     */
+    function ages(str)
+    {
+        var   r   =   str.match(/^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/);
+        if(r==null)return   false;
+        var   d=new Date(r[1],r[3]-1, r[4]);
+        if (d.getFullYear()==r[1]&&(d.getMonth()+1)==r[3]&&d.getDate()==r[4])
+        {
+            var   y=new Date().getFullYear();
+            var years =y-r[1];
+            if(years<1)
+                years = 1;
+            return years;
+        }
+        return(false);
     }
 }();

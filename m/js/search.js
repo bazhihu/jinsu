284adfd44c1a7be8216444274808de45e2050025
huzhiqang <1123456>
2015-07-02 12:28:32 +0800

    搜索

--- m/js/search.js
+++ m/js/search.js
@@ -8,6 +8,9 @@
 
     var searchTimeout;
 
+    var q = getUrlQueryString('q');
+    console.log(q);
+
     if (input && history && list) {
         function onInput() {
             if (!input.value) {
@@ -56,39 +59,43 @@ console.log(input.value);
                 timeout:30000,
                 success: function(data){
                     if(data.code == 200){
-                        console.log(data.data);
                         var items = data.data.items,
                             length = items.length,
                             list = $('#list');
                         list.empty();
-                        for(var i = 0;i<length;i++){
-                            var result = '<li>' +
-                                '<a href="./nursesDetail.html?worker_id='+items[i]['worker_id']+'&start_time=0&end_time=0&in_service='+items[i]['in_service']+'">' +
-                                '<div class="nurses-photo" style="background-image: url('+items[i]['pic']+')"></div>' +
-                                '<div class="nurses-detail">' +
-                                '<span class="nurses-price"><em>'+items[i]['price']+'</em>元/天</span>';
-                            if(items[i].star>4 || !items[i].star){
-                                result+='<span class="nurses-rate">★★★★★</span>';
-                            } else if(items[i].star>3){
-                                result+='<span class="nurses-rate">★★★★</span>';
-                            }else if(items[i].star>2){
-                                result+='<span class="nurses-rate">★★★</span>';
-                            }else if(items[i].star>1){
-                                result+='<span class="nurses-rate">★★</span>';
-                            }else if(items[i].star>0){
-                                result+='<span class="nurses-rate">★</span>';
-                            }
-                            result +=
-                                '<h4 class="nurses-name">'+items[i]['name']+'</h4>' +
-                                '<p class="nurses-intro">'+ages(items[i].birth)+'岁 | '+items[i].native_province+' | '+ages(items[i].start_work)+'年从业经验</p>' +
-                                '<p class="nurses-reserve">'+items[i].level_name;
-                            if(items[i].in_service)
-                                result+='<span class="status-busy nurses-status">服务中</span>';
-                            else
-                                result+='<span class="nurses-status">空闲</span>';
+                        if(length>0){
+                            for(var i = 0;i<length;i++){
+                                var result = '<li>' +
+                                    '<a href="./nursesDetail.html?worker_id='+items[i]['worker_id']+'&start_time=0&end_time=0&in_service='+items[i]['in_service']+'">' +
+                                    '<div class="nurses-photo" style="background-image: url('+items[i]['pic']+')"></div>' +
+                                    '<div class="nurses-detail">' +
+                                    '<span class="nurses-price"><em>'+items[i]['price']+'</em>元/天</span>';
+                                if(items[i].star>4 || !items[i].star){
+                                    result+='<span class="nurses-rate">★★★★★</span>';
+                                } else if(items[i].star>3){
+                                    result+='<span class="nurses-rate">★★★★</span>';
+                                }else if(items[i].star>2){
+                                    result+='<span class="nurses-rate">★★★</span>';
+                                }else if(items[i].star>1){
+                                    result+='<span class="nurses-rate">★★</span>';
+                                }else if(items[i].star>0){
+                                    result+='<span class="nurses-rate">★</span>';
+                                }
+                                result +=
+                                    '<h4 class="nurses-name">'+items[i]['name']+'</h4>' +
+                                    '<p class="nurses-intro">'+ages(items[i].birth)+'岁 | '+items[i].native_province+' | '+ages(items[i].start_work)+'年从业经验</p>' +
+                                    '<p class="nurses-reserve">'+items[i].level_name;
+                                if(items[i].in_service)
+                                    result+='<span class="status-busy nurses-status">服务中</span>';
+                                else
+                                    result+='<span class="nurses-status">空闲</span>';
 
-                            result+='</div></a></li>';
-                            list.append(result);
+                                result+='</div></a></li>';
+                                list.append(result);
+                            }
+                        }else{
+                            var result = '<div class="nurses-list-none nurses-list"></div>';
+                            $('#list').append(result);
                         }
                         /*list.querySelector('ul').innerHTML = result;*/
                         //window.location.reload();
